e098deee0085ee16b9fb784e309705e19a2477ca
vesran <tran.yves@gmail.com>
2020-04-18 13:35:50 +0200

    add parameters

--- graphit/viz.py
+++ graphit/viz.py
@@ -2,11 +2,14 @@ import matplotlib.pyplot as plt
 from graphit.graphcore import Graph
 
 
-def show(graph):
+def show(graph, markersize=20, annotate=True):
     data = [[edges.v1.x, edges.v1.y, edges.v2.x, edges.v2.y] for edges in graph.edges]
     for edge in data:
-        plt.plot((edge[0], edge[2]), (edge[1], edge[3]), 'ro-')
+        plt.plot((edge[0], edge[2]), (edge[1], edge[3]), 'ro-', markersize=markersize)
 
+    if annotate:
+        for vertex in graph.vertices:
+            plt.annotate(vertex.id, (vertex.x, vertex.y))
     plt.show()
 
 
