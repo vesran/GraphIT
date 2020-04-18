016c9d49e57adec470719529446a8f3ff26f8618
vesran <tran.yves@gmail.com>
2020-04-18 12:29:50 +0200

    code structure

---
+++ graphit/graphcore.py
@@ -0,0 +1,21 @@
+
+class Graph:
+
+    def __init__(self):
+        pass
+
+
+class Vertex:
+
+    def __init__(self):
+        pass
+
+
+class Edge:
+
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
